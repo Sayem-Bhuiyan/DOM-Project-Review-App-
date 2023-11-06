ea05bc18fb2cb3725d75269d1f04966be39a9cfb
Sayem <sayem465508@gmail.com>
2023-11-06 13:14:00 +0600

    added enter button action

--- index.js
+++ index.js
@@ -6,9 +6,28 @@ document.getElementById('btn').addEventListener('click', function(){
     // create new element 
     const newReview = document.createElement('p');
     newReview.innerText = inputData;
-    
+
     // add new review to the container
     const reviewContainer = document.getElementById('container');
     reviewContainer.appendChild(newReview);
     input.value = '';
+})
+
+document
+.getElementById('text-box')
+.addEventListener('keyup', function(event){
+    if(event.key === 'Enter'){
+         // select the input and set the input data
+        const input = document.getElementById('text-box');
+        const inputData = input.value;
+
+        // create new element 
+        const newReview = document.createElement('p');
+        newReview.innerText = inputData;
+
+        // add new review to the container
+        const reviewContainer = document.getElementById('container');
+        reviewContainer.appendChild(newReview);
+        input.value = '';
+    }
 })
